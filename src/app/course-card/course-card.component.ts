42b67abadb00af53532ee87cf27e288dc72b43d9
jskorups <skorupski.jakub@gmail.com>
2025-02-09 20:12:06 +0100

    *ngIf

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,9 +1,10 @@
-import { Component, input, output } from "@angular/core";
+import { Component, computed, input, output } from "@angular/core";
 import { Course } from "./model/course";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "course-card",
-  imports: [],
+  imports: [CommonModule],
   templateUrl: "./course-card.component.html",
   styleUrl: "./course-card.component.css",
 })
@@ -11,6 +12,10 @@ export class CourseCardComponent {
   readonly course = input<Course>();
   courseSelected = output<Course>();
   readonly index = input.required<number>();
+
+  isIconUrl = computed(() => {
+    return this.course().iconUrl && this.course();
+  });
 
   onCourseViewed() {
     console.log("kespa");
