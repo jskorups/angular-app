d3f6ccc4ac3e57ddd2291a7d3d74691e0e3bf441
jskorups <skorupski.jakub@gmail.com>
2025-02-09 18:25:57 +0100

    inputs outputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { COURSES } from "../db-data";
+import { Course } from "./course-card/model/course";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,11 @@ import { COURSES } from "../db-data";
   styleUrls: ["./app.component.css"],
   standalone: false,
 })
-export class AppComponent {}
+export class AppComponent {
+  onCourseSelected(course: Course) {
+    console.log("App component clicked", course);
+  }
+  course1 = COURSES[0];
+  course2 = COURSES[1];
+  course3 = COURSES[2];
+}
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from "@angular/core";
+import { Course } from "./model/course";
 
 @Component({
-  selector: 'course-card',
+  selector: "course-card",
   imports: [],
-  templateUrl: './course-card.component.html',
-  styleUrl: './course-card.component.css'
+  templateUrl: "./course-card.component.html",
+  styleUrl: "./course-card.component.css",
 })
 export class CourseCardComponent {
+  readonly course = input<Course>();
+  courseSelected = output<Course>();
 
+  onCourseViewed() {
+    console.log("kespa");
+    this.courseSelected.emit(this.course());
+  }
 }
